6ad069981d73c7118f12aff39a16512638e5db70
Shiroi Negai <shiroinegai@pm.me>
2021-08-18 06:50:40 +0800

    Implement several basic grid functions
    Namely, addCell, colourCell and generateGrid

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 console.log("Etch-a-Sketch is running.");
+
+const root = document.documentElement;
+
+const editGridButton = document.querySelector(".edit-grid");
+
+const gridContainer = document.querySelector(".grid-container");
+
+addCell = () => {
+  let newCell = document.createElement("div");
+  newCell.addEventListener("mouseover", (e) => colourCell(e.target));
+  gridContainer.appendChild(newCell);
+};
+
+colourCell = (cell) => {
+  cell.style.backgroundColor = "#ffffff";
+};
+
+generateGrid = () => {
+  gridContainer.innerHTML = "";
+  size = getComputedStyle(root).getPropertyValue("--grid-size");
+  size *= size;
+  for (let i = 0; i < size; i++) {
+    addCell();
+  }
+};
+
+const clearGridButton = document.querySelector(".clear-grid");
+clearGridButton.addEventListener("click", generateGrid);
+
+generateGrid();
